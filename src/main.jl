137696426b3fd5fdf5d50be98020b6322765ef6b
Matthew Scott <m.valckescott@gmail.com>
2023-04-22 17:27:32 -0700

    Add config; ignore quotes option; working from command line.

--- src/main.jl
+++ src/main.jl
@@ -1,8 +1,10 @@
+using YAML
+
 include("./objects/index.jl")
 include("utils.jl")
 include("unroll.jl")
 
-function main(input_folder::String, longform_file::String, outputfolder::String; texfilesfolder="./latex_files/", imgfilefolder="Files")
+function main(input_folder::String, longform_file::String, outputfolder::String, configfile="./examples/config.YAML"; texfilesfolder="./latex_files/", imgfilefolder="Files")
     if !isdir(outputfolder)
         mkdir(outputfolder)
     end
@@ -23,6 +25,7 @@ function main(input_folder::String, longform_file::String, outputfolder::String;
 
     metadata, unrolledcontent = unrolledmainfile(input_folder, longform_file; filefolder=imgfilefolder, outfolder=outputfolder)
     abstract = find_heading_content(unrolledcontent, "Abstract"; removecontent=true)
+    merge!(metadata, YAML.load_file(configfile))
 
     f = open(joinpath(outputfolder, "output.tex"), write=true, create=true)
     write(
@@ -49,22 +52,43 @@ function main(input_folder::String, longform_file::String, outputfolder::String;
     )
     #end
     close(f)
-    #parsed_notes = parse_obsidian_folder(joinpath(input_folder))
-    #start_state = State(parsed_notes, input_folder, longform_file, metadata)
-    #latex = build_latex(start_state)
-    #write(output_file, latex)
+    @info "Export Completed!"
 end
 
+if !(length(ARGS) in [3, 4])
+    println("Usage: julia main.jl <input_folder> <longform_file> <output_file>[ <config_file>]")
+end
 
-#Markdown.@trigger
+if length(ARGS) == 4
+    scriptconfig = YAML.load_file(ARGS[4])
+    if scriptconfig["ignore_quotes"]
+        @info "Ignoring quotes from config"
+        eval(quote
+            import Markdown: BlockQuote
+            function latex(io::IO, md::BlockQuote)
+                return ""
+            end
+        end)
+    end
+end
 
-if length(ARGS) != 3
-    println("Usage: julia main.jl <input_folder> <longform_file> <output_file>")
-else
-    main(args[1], args[2], args[3])
+main(ARGS...)
+
+#=
+scriptconfig = YAML.load_file(ARGS[4])
+if scriptconfig["ignore_quotes"]
+    @info "Ignoring quotes from config"
+    eval(quote
+        import Markdown: BlockQuote
+        function latex(io::IO, md::BlockQuote)
+            println("Ignoring quote")
+            return ""
+        end
+    end)
 end
 
 #main("../../Ik-Vault/Zettelkasten/", "Sub-Gaussian McDiarmid Inequality and Classification on the Sphere", "./examples/output/project555_output/")
 main("./examples/", "main_note", "./examples/output/example_output/"; texfilesfolder="./latex_files/")
 #main("../../myVault/Zettelkasten/", "Journal Sample Longform", "./examples/output/journal1/")
 #main("./myVault/Zettelkasten/", "./myVault/Zettelkasten/Uneven Sampling Journal Version Longform.md", "./export_markdown/Obsidian\ Paper\ Export/examples/output/uneven_journal/"; output_file_name="Uneven Sampling Journal Version.tex", img_folder_name="Files")
+=#
